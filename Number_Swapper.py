f10ee1d9ae542d20726c1f3ffd0cf289f6c2e6e8
Shamanth Krishna <98342024+Shamanthkrishna@users.noreply.github.com>
2024-07-10 19:21:37 +0530

    Add files via upload

---
+++ Number_Swapper.py
@@ -0,0 +1,15 @@
+def num_swap():
+    a = int(input("Enter the First Number:"))
+    b = int(input("Enter the Second Number:"))
+    print(f"Numbers Before Swapping:{a},{b}")
+    c = a
+    a = b
+    b = c
+    print(f"Numbers After Swapping:{a},{b}")
+
+while True:
+    num_swap()
+    try_again = input("Do you want try again? (y/n):").lower()
+    if try_again != "y":
+        break
+print("Thank You")
